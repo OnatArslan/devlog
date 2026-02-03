c661cb7f22b6b012136f27f982daf7c0cf58dd04
OnatArslan <onatarslan133@gmail.com>
2026-02-03 07:43:51 +0300

    Commit for changes

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,5 +1,46 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/OnatArslan/devlog/internal/httpx"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/joho/godotenv"
+)
+
 func main() {
+	ctx := context.Background()
+
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+
+	fmt.Println(ctx) // We need to remove it later
+
+	// A good base middleware stack
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	// Set a timeout value on the request context (ctx), that will signal
+	// through ctx.Done() that the request has timed out and further
+	// processing should be stopped.
+	r.Use(middleware.Timeout(20 * time.Second))
+
+	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
+		httpx.WriteJSON(w, http.StatusOK, "OK")
+	})
+
+	http.ListenAndServe(os.Getenv("PORT"), r)
 
 }

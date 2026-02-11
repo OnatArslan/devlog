7dadae38a3fa4b496d39df1b69c93dae752ba55c
OnatArslan <onatarslan133@gmail.com>
2026-02-11 19:41:41 +0300

    feat(post) update started

--- db/query/posts.sql
+++ db/query/posts.sql
@@ -12,3 +12,12 @@ SELECT p.*, u.username FROM posts p JOIN users u ON u.id = p.author_id;
 -- name: GetPostById :one
 SELECT p.*, u.username FROM posts p JOIN users u ON u.id = p.author_id
 WHERE p.id = $1;
+
+-- name: UpdatePost :one
+UPDATE posts
+SET
+  title = COALESCE(sqlc.narg(title), title),
+  content = COALESCE(sqlc.narg(content), content),
+  updated_at = now()
+WHERE id = $1
+RETURNING *;
